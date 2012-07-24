3fbecc3cd2e7b26f84bb143288a1eaaf54ff48e5
Anand Hegde <anandghegde@gmail.com>
2012-07-25 00:33:41 +0530

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'boot.rb'))
+
+Delayed::Backend::Mongoid::Job.create_indexes
+
